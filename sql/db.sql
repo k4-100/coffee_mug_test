e0d5a01661ef870a147f56d07c441431d9a21c7b
k4-100 <k4-100@protonmail.com>
2022-11-15 00:07:13 +0100

    Added some basic structure to sql

---
+++ sql/db.sql
@@ -0,0 +1,18 @@
+
+
+CREATE DATABASE ProductsDB;
+USE ProductsDB;
+
+
+CREATE TABLE ProductsTAB(
+  Id INT NOT NULL AUTO_INCREMENT,
+  Name VARCHAR(255) NOT NULL,
+  Price FLOAT NOT NULL, 
+  UpdateDate DATETIME NOT NULL DEFAULT NOW(),
+  PRIMARY KEY(Id)
+);
+
+
+INSERT INTO ProductsTAB(Name, Price)
+VALUES ("apple", 3.01);
+
