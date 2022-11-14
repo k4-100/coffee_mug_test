75c2b3ac4beb111516e3c7fb9ffd35087d29f72e
k4-100 <k4-100@protonmail.com>
2022-11-15 00:14:43 +0100

    Added basic rpc path

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,32 @@
 import express from "express";
+import mysql from "mysql";
 
 const app = express();
-const PORT = 5000;
-const HOSTNAME = "127.0.0.1";
+const PORT: number = 5000;
+const HOSTNAME: string = "127.0.0.1";
+
+const mysqlConnection = mysql.createConnection({
+  host: HOSTNAME,
+  user: "root",
+  password: "1234",
+  database: "ProductsDB",
+});
+
+mysqlConnection.connect((err) => {
+  if (err) throw err;
+  console.log("Connected to mysql database");
+});
+
+app.get("/api/v1/products", (req, res) => {
+  mysqlConnection.query("SELECT * FROM ProductsTAB;", (err, result) => {
+    if (err) throw err;
+
+    return res.status(200).json({
+      data: result,
+      status: true,
+    });
+  });
+});
 
 app.get("/api/v1", (req, res) => {
   res.status(200).json({
